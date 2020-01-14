b8e1f18a93e5bd502d0779aff2e130fc3e47fa19
Paul Lysak <paul.lysak@gmail.com>
2020-01-14 22:14:48 +0200

    Open classes, parameter untupling

--- dotty-features-sample/src/test/scala/meetup/Test1.scala
+++ dotty-features-sample/src/test/scala/meetup/Test1.scala
@@ -1,3 +1,5 @@
+package meetup
+
 import org.junit.Test
 import org.junit.Assert._
 
---
+++ dotty-features-sample/src/test/scala/meetup/TupledFunctionsSpec.scala
@@ -0,0 +1,11 @@
+package meetup
+
+import org.junit.Test
+import org.junit.Assert._
+
+class TupledFunctionsSpec {
+  @Test def t1(): Unit = {
+    println("hi there")
+//    assertEquals("I was compiled by dotty :)", Main.msg)
+  }
+}
